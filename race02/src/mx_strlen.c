33c7647f0a5a8650510d580bafded83bdfa6030b
mtereshyn <maksym.tereshyn@nure.ua>
2019-11-23 21:08:48 +0200

    all_before
    ..

---
+++ race02/src/mx_strlen.c
@@ -0,0 +1,12 @@
+#include "header.h"
+
+int mx_strlen(const char *s) {
+        int count = 0;
+
+        while (*s != '\0') {
+                count++;
+                s++;
+        }
+
+        return count;
+}
